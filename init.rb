c643485a8beee537244ff353b6cef245ab8ba212
Ondrej Zabojnik <zabojnik@dextronet.com>
2014-09-08 12:46:55 +0200

    Fix paths

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
-Redmine::Plugin.register :deadlines do
-  name 'Deadlines'
+Redmine::Plugin.register :redmine_deadlines do
+  name 'Redmine Deadlines'
   author 'Ondrej Zabojnik'
   description 'Track deadlines.'
   version '0.0.1'
-  url 'https://github.com/zabojnik/redmine-deadlines'
-  menu :top_menu, :deadlines, { :controller => 'deadlines', :action => 'index' }, :caption => 'Deadlines', :after => :home
+  url 'https://github.com/zabojnik/redmine_deadlines'
+  menu :top_menu, :redmine_deadlines, { :controller => 'deadlines', :action => 'index' }, :caption => 'Deadlines', :after => :home
 end
